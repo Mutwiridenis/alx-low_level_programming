1fd5e21ceaf9083f3d301c25f96779e1e2658bbb
Mutwiridenis <deeih94@gmail.com>
2022-03-10 20:12:07 -0800

    4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 #include <stdion.h>
+
 /**
+
  *main - entry point
+
  *
+
  *Return: 0 after printing the function
+
 */
+
 int main(void)
+
 {
+
 puts("\"Programming is like building a multillingual puzzle");
+
 return (0);
+
 }
