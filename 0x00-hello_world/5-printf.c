20e2af72834488058d5193a0997048deeba68045
Mutwiridenis <deeih94@gmail.com>
2022-03-13 15:16:18 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,10 @@
 /**
  *main - Entry point
  *My hello, printf task
+ *Return: return 1 after program executes
 */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
