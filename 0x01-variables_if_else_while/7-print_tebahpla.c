622733eabf4b32a9f74beb79f8e91c5217b743f5
Mutwiridenis <deeih94@gmail.com>
2022-03-13 18:40:28 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Succes/correct)
+*/
+
+int main(void)
+
+{
+
+for(char letters 50 ; letters >= 25; letters--)
+{
+putchar(letters)
+}
+
+putchar('\n');
+
+return (0);
+
+}
