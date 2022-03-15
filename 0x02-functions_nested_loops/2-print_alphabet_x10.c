31c21926de5fd70ffcd40866a431087d1148319c
Mutwiridenis <deeih94@gmail.com>
2022-03-15 12:10:12 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ *  Return: nothing
+*/
+int main (void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; ++i)
+        {
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
+
