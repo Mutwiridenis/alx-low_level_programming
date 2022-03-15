cdcd284ed60f9d7b243e7eeaccdbd4e9ab97a403
Mutwiridenis <deeih94@gmail.com>
2022-03-15 13:47:52 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,17 +5,14 @@
  *  Return: Always 0 (Success)
 */
 void print_alphabet(void)
-
 {
-	char ch;
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for ( c ='a'; c <= 'z'; c++);
 	{
-		printf("%c", ch);
+		_putchar(c);
 	}
 
-        print_alphabet();
-	return (0);
-
-
+	_putchar('\n');
 }
+			
