b5431bdc90f77901c11632554eaba9dd4751527e
Mutwiridenis <deeih94@gmail.com>
2022-03-15 15:32:09 -0700

    m

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,45 +1,23 @@
-#include "main.h"
+int _putchar(char c);
 
+void print_alphabet(void);
 
+void print_alphabet_x10(void);
 
+int _islower(int c);
 
+int _isalpha(int c);
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: nothing, return void
- *
- *     */
+int print_sign(int n);
 
+int _abs(int);
 
+int print_last_digit(int);
 
-int main(void)
+void jack_bauer(void);
 
-{
+void times_table(void);
 
-	_putchar('_');
+int add(int, int);
 
-	_putchar('p');
-
-	_putchar('u');
-
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-
-	_putchar('\n');
-
-
-
-	return (0);
-
-	
+void print_to_98(int n);
