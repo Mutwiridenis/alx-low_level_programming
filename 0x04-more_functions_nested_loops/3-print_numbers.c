75c85649ec2019060d02c245cd84558c842cb61e
Mutwiridenis <deeih94@gmail.com>
2022-03-17 14:03:48 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers 0 t0 9
+ *
+ * Return: Always 0
+*/
+void print_numbers(void)
+{
+      int i;
+
+      for (i = 0; i <=9; i++)
+      {
+              _putchar('0' +i);
+
+       }
+       _putchar('\n');
+}
