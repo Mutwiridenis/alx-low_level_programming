ff2ed548e9662b2c462dddff729e7a6f01378162
Mutwiridenis <deeih94@gmail.com>
2022-03-17 17:56:51 -0700

    commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,6 @@ void print_triangle(int size)
 {
 
 
-
 int i;
 int z;
 int d;
@@ -19,7 +18,7 @@ int p;
 if (size > 0)
 {
 d = size - 1;
-for (i = 0; i < size; i++)
+for (i = 0; i < size ; i++)
 {
 for (z = d; z > 0 ; z--)
 {
@@ -28,7 +27,6 @@ _putchar (' ');
 
 for (p = 0; p <= i; p++)
 {
-
 _putchar (35);
 }
 
@@ -36,9 +34,11 @@ d--;
 
 _putchar ('\n');
 
+
 }
 
 }
+
 else
 {
 _putchar ('\n');
